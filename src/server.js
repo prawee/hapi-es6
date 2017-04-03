48984055365f5f480c7cf56687a61d9d021a507e
Prawee Wongsa <prawee@hotmail.com>
2017-04-03 19:24:33 +0700

    update server and using rest-hapi plugin

--- src/server.js
+++ src/server.js
@@ -3,39 +3,47 @@
  */
 import Hapi from 'hapi';
 import Inert from 'inert';
-
+import RestHapi from 'rest-hapi';
+import Mongoose from 'mongoose';
 /**
  * Create server instance and make connection
  */
 const server = new Hapi.Server();
 server.connection({
-  port: 8080
+  port: 8080,
+  host: 'localhost'
 });
 
 /**
  * Plugin and Route 
  */
-server.register(Inert, (err) => {
+server.register([Inert, {
+  register: RestHapi,
+  options: {
+    mongoose: Mongoose
+  }
+}
+], (err) => {
   if (err) {
     throw err;
   }
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-      // reply('Hello World!');
-      reply.file('./public/index.html');
-    }
-  });
-});
+  // server.route({
+  //   method: 'GET',
+  //   path: '/',
+  //   handler: (request, reply) => {
+  //     reply.file('./public/index.html');
+  //   }
+  // });
 
-/**
+  /**
  * Start services
  */
-server.start(err => {
-  if (err) {
-    console.error('Error was handled!');
-    console.error(err);
-  }
-  console.log(`Server started at ${server.info.uri}`);
+  server.start(err => {
+    if (err) {
+      console.error('Error was handled!');
+      console.error(err);
+    }
+    console.log(`Server started at ${server.info.uri}`);
+  });
+
 });
