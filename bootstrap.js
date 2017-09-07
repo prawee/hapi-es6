f84dce491f626b20339badd3af7b32c277e1a9ce
Prawee Wongsa <prawee.w@integra8t.com>
2017-09-07 12:13:15 +0700

    added file header

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,8 @@
+/*
+ * @Author: Prawee Wongsa <prawee@hotmail.com>
+ * @Date: 2017-03-24 12:05:31 
+ * @Last Modified by: Prawee Wongsa
+ * @Last Modified time: 2017-09-07 12:07:12
+ */
 require('babel-core/register');
 require('./src/server');
